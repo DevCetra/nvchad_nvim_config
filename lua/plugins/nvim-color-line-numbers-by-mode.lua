22eeb65da531a584470a5ed9c22c737993a6d9d0
robmllze <86869208+robmllze@users.noreply.github.com>
2024-11-20 17:23:06 +1100

    update

--- lua/plugins/nvim-color-line-numbers-by-mode.lua
+++ lua/plugins/nvim-color-line-numbers-by-mode.lua
@@ -1,6 +1,7 @@
 return {
   {
     "DevCetra/nvim-color-line-numbers-by-mode",
+    lazy = false, --load during startup
     config = function()
       require("nvim-color-line-numbers-by-mode").setup()
     end,
