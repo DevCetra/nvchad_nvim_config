cd2cca48f6ef98439365e2e8785fae6a50df79e4
robmllze <86869208+robmllze@users.noreply.github.com>
2024-11-29 23:07:45 +1100

    update

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+-- Set up cache and leader key
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
 vim.g.mapleader = " "
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+-- Bootstrap lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
@@ -11,50 +12,47 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+-- Load custom lazy configuration (optional)
+local lazy_config = require("configs.lazy")
 
--- load plugins
+-- Set up the plugins
 require("lazy").setup({
   {
     "NvChad/NvChad",
     lazy = false,
     branch = "v2.5",
-    import = "nvchad.plugins",
+    import = "nvchad.plugins",  -- Import plugins from NvChad
   },
-
-  { import = "plugins" },
+  { import = "plugins" },  -- Custom plugins
 }, lazy_config)
 
--- load theme
+-- Load theme and custom defaults
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
--- Set textwidth
-vim.o.textwidth = 100
--- Set colorcolumn (for visual guide)
-vim.o.colorcolumn = "80,100"
--- Enable absolute line numbers
-vim.wo.number = true
--- Enable relative line numbers
+-- Set some basic editor settings
+vim.o.textwidth = 100         -- Set textwidth for wrapping
+vim.o.colorcolumn = "80,100"  -- Visual guide for lines
+vim.wo.number = true          -- Enable absolute line numbers
+vim.wo.relativenumber = true  -- Enable relative line numbers
 
--- Automatically open NvimTree when Neovim starts.
-vim.api.nvim_create_autocmd(
-  "VimEnter", {
-    callback = function()
-      require("nvim-tree.api").tree.open()
-    end,
-  })
+-- Automatically open NvimTree when Neovim starts
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("nvim-tree.api").tree.open()
+  end,
+})
 
--- Setup nvim-tree.
+-- NvimTree setup with custom keymaps
 local function my_on_attach(bufnr)
   local api = require "nvim-tree.api"
-
   local function opts(desc)
     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
-  -- Default mappings.
+
+  -- Default mappings from nvim-tree
   api.config.mappings.default_on_attach(bufnr)
-  -- Custom mappings.
+  -- Custom mappings
   vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
   vim.keymap.set('n', '\\', api.tree.change_root_to_parent, opts('Up'))
   vim.keymap.set('n', '<C-CR>', function()
@@ -64,24 +62,41 @@ local function my_on_attach(bufnr)
     end
   end, opts('Down'))
 end
+
 require("nvim-tree").setup({
   view = {
-    width = 60,
+    width = 60,  -- Set width of NvimTree
   },
-  on_attach = my_on_attach,
+  on_attach = my_on_attach,  -- Apply custom keymaps
 })
 
+-- Load other settings from 'options' and 'nvchad.autocmds'
 require "options"
 require "nvchad.autocmds"
 
-vim.schedule(
-  function()
-    require "mappings"
-  end
-)
+-- Schedule the loading of mappings (e.g., key bindings)
+vim.schedule(function()
+  require "mappings"
+end)
 
--- Modify IndentBlankline color to a very light "transparent" white
+-- Modify IndentBlankline colors to a very light "transparent" white
 vim.cmd [[
   highlight IndentBlanklineChar guifg=#F0F0F0 ctermbg=0
   highlight IndentBlanklineContextChar guifg=#E0E0E0 ctermbg=0
 ]]
+
+-- Setup LSP diagnostics to disable inline warnings and errors
+local function setup_lsp_diags()
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    {
+      virtual_text = false,         -- Disable inline diagnostic messages
+      signs = true,                 -- Enable diagnostic signs in the gutter
+      update_in_insert = false,     -- Do not update diagnostics in insert mode
+      underline = true,             -- Underline text with diagnostics
+    }
+  )
+end
+
+-- Call the LSP diagnostics setup
+setup_lsp_diags()
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,283 +1,96 @@
 require 'nvchad.mappings'
 
+
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 --------------------------------------------------------------------------------
 -- General Mappings
 --------------------------------------------------------------------------------
 
--- Exit terminal mode using 'C-w'..
-map(
-  't',
-  '<C-w>',
-  '<C-\\><C-N>',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+-- Toggle line diagnostics.
+map('n', '<Leader>xt', function() vim.diagnostic.open_float({ scope = "line" }) end, opts)
 
--- Exit insert mode using 'jk' (while in insert mode).
-map(
-  { 'i' },
-  'jk',
-  '<Esc>',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+-- Exit terminal mode using 'C-w'.
+map('t', '<C-w>', '<C-\\><C-N>', opts)
+
+-- Exit insert mode using 'jk'.
+map('i', 'jk', '<Esc>', opts)
 
 -- F13: Enter Normal mode (from any mode).
-map(
-  { 'i', 'n', 'v', 'c', 't' },
-  '<F13>',
-  '<Esc> ',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+map({ 'i', 'n', 'v', 'c', 't' }, '<F13>', '<Esc>', opts)
 
 -- F14: Enter Insert mode (from any mode).
-map(
-  { 'i', 'n', 'v', 'c', 't' },
-  '<F14>',
-  '<Esc>i',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+map({ 'i', 'n', 'v', 'c', 't' }, '<F14>', '<Esc>i', opts)
 
 -- F15: Enter Visual mode (from any mode).
-map(
-  { 'i', 'n', 'v', 'c', 't' },
-  '<F15>',
-  '<Esc>v',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+map({ 'i', 'n', 'v', 'c', 't' }, '<F15>', '<Esc>v', opts)
 
 -- F16: Enter Command mode (from any mode).
-map(
-  { 'i', 'n', 'v', 'c', 't' },
-  '<F16>',
-  '<Esc><Cmd>call feedkeys(":")<CR>',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+map({ 'i', 'n', 'v', 'c', 't' }, '<F16>', '<Esc><Cmd>call feedkeys(":")<CR>', opts)
 
 -- F19: Exit Terminal mode.
-map(
-  't',
-  '<F19>',
-  'exit<CR>',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+map('t', '<F19>', 'exit<CR>', opts)
 
 -- F19: Write out current buffer (while in insert mode).
-map(
-  'i',
-  '<F19>',
-  '<Esc>:w<CR>a',
-  {
-    noremap = true,
-    silent = true,
-  }
-)
+map('i', '<F19>', '<Esc>:w<CR>a', opts)
 
 --------------------------------------------------------------------------------
 -- LSP Mappings
 --------------------------------------------------------------------------------
 
 -- Refactor a symbol.
-map(
-  { 'n', 'v' },
-  '<Leader>rs',
-  '<cmd>lua vim.lsp.buf.rename()<CR>',
-  {
-    desc = 'Refactor a symbol',
-    noremap = true,
-    silent = true,
-  }
-)
+map({ 'n', 'v' }, '<Leader>rs', '<cmd>lua vim.lsp.buf.rename()<CR>', { desc = 'Refactor a symbol', unpack(opts) })
 
 -- Replace text using regex pattern.
-map(
-  { 'n', 'v' },
-  '<Leader>rr',
-  '<cmd>lua ReplaceUsingRegex()<CR>',
-  {
-    desc = 'Replace text using regex',
-    noremap = true,
-    silent = true,
-  }
-)
+map({ 'n', 'v' }, '<Leader>rr', '<cmd>lua ReplaceUsingRegex()<CR>', { desc = 'Replace text using regex', unpack(opts) })
 
 -- Show hover information.
-map(
-  'n',
-  '<Leader>I',
-  '<cmd>lua vim.lsp.buf.hover()<CR>',
-  {
-    desc = 'Show hover information',
-    noremap = true,
-    silent = true,
-  }
-)
+map('n', '<Leader>I', '<cmd>lua vim.lsp.buf.hover()<CR>', { desc = 'Show hover information', unpack(opts) })
 
 -- Go to the definition of a symbol.
-map(
-  'n',
-  '<Leader>gd',
-  '<cmd>lua vim.lsp.buf.definition()<CR>',
-  {
-    desc = 'Go to the definition',
-    noremap = true,
-    silent = true,
-  }
-)
+map('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { desc = 'Go to the definition', unpack(opts) })
 
 -- Find references to a symbol.
-map(
-  'n',
-  '<Leader>gr',
-  '<cmd>lua vim.lsp.buf.references()<CR>',
-  {
-    desc = 'Find references',
-    noremap = true,
-    silent = true,
-  }
-)
-
-function ReplaceUsingRegex()
-  -- Prompt for the regex pattern
-  local pattern = vim.fn.input('Regex pattern to replace: ')
-
-  -- Prompt for the replacement string
-  local replacement = vim.fn.input('Replacement: ')
-
-  -- If both pattern and replacement are provided, perform the replacement
-  if pattern ~= '' and replacement ~= '' then
-    -- Use 'substitute' command with valid pattern and replacement
-    local cmd = string.format('%%s/%s/%s/g', vim.fn.escape(pattern, '/'), vim.fn.escape(replacement, '/'))
-    vim.cmd(cmd)
-  end
-end
+map('n', '<Leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', { desc = 'Find references', unpack(opts) })
 
 --------------------------------------------------------------------------------
--- NVIM DAP (Debug Adapter Protocol) Mappings
+-- NVIM DAP Mappings
 --------------------------------------------------------------------------------
 
 -- Step into the debugger.
-map(
-  'n',
-  '<Leader>dl',
-  '<cmd>lua require"dap".step_into()<CR>',
-  {
-    desc = 'Debugger step into',
-  }
-)
+map('n', '<Leader>dl', '<cmd>lua require"dap".step_into()<CR>', { desc = 'Step into', unpack(opts) })
 
 -- Step over in the debugger.
-map(
-  'n',
-  '<Leader>dj',
-  '<cmd>lua require"dap".step_over()<CR>',
-  {
-    desc = 'Debugger step over',
-  }
-)
+map('n', '<Leader>dj', '<cmd>lua require"dap".step_over()<CR>', { desc = 'Step over', unpack(opts) })
 
 -- Step out of the debugger.
-map(
-  'n',
-  '<Leader>dk',
-  '<cmd>lua require"dap".step_out()<CR>',
-  {
-    desc = 'Debugger step out',
-  }
-)
+map('n', '<Leader>dk', '<cmd>lua require"dap".step_out()<CR>', { desc = 'Step out', unpack(opts) })
 
 -- Continue execution in the debugger.
-map(
-  'n',
-  '<Leader>dc',
-  '<cmd>lua require"dap".continue()<CR>',
-  {
-    desc = 'Debugger continue',
-  }
-)
+map('n', '<Leader>dc', '<cmd>lua require"dap".continue()<CR>', { desc = 'Continue', unpack(opts) })
 
 -- Toggle a breakpoint in the debugger.
-map(
-  'n',
-  '<Leader>db',
-  '<cmd>lua require"dap".toggle_breakpoint()<CR>',
-  {
-    desc = 'Debugger toggle breakpoint',
-  }
-)
+map('n', '<Leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>', { desc = 'Toggle breakpoint', unpack(opts) })
 
 -- Set a conditional breakpoint.
-map(
-  'n',
-  '<Leader>dd',
-  '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>',
-  {
-    desc = 'Debugger set conditional breakpoint',
-  }
-)
+map('n', '<Leader>dd', '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', { desc = 'Set conditional breakpoint', unpack(opts) })
 
 -- Terminate the debugger.
-map(
-  'n',
-  '<Leader>de',
-  '<cmd>lua require"dap".terminate()<CR>',
-  {
-    desc = 'Debugger terminate',
-  }
-)
+map('n', '<Leader>de', '<cmd>lua require"dap".terminate()<CR>', { desc = 'Terminate debugger', unpack(opts) })
 
 -- Run the last debugging session.
-map(
-  'n',
-  '<Leader>dr',
-  '<cmd>lua require"dap".run_last()<CR>',
-  {
-    desc = 'Debugger run last',
-  }
-)
+map('n', '<Leader>dr', '<cmd>lua require"dap".run_last()<CR>', { desc = 'Run last session', unpack(opts) })
 
 --------------------------------------------------------------------------------
 -- Rust Mappings
 --------------------------------------------------------------------------------
 
 -- Show testable items in Rust.
-map(
-  'n',
-  '<Leader>dt',
-  '<cmd>lua vim.cmd("RustLsp testables")<CR>',
-  {
-    desc = 'Debugger testables',
-  }
-)
+map('n', '<Leader>dt', '<cmd>lua vim.cmd("RustLsp testables")<CR>', { desc = 'Show testables', unpack(opts) })
 
 --------------------------------------------------------------------------------
 -- Git Mappings
 --------------------------------------------------------------------------------
 
-map(
-  'n',
-  '<Leader>gh',
-  ':Gitsigns preview_hunk<CR>',
-  {}
-)
+map('n', '<Leader>gh', ':Gitsigns preview_hunk<CR>', opts)
